a546522d3515bc3faff3e3a8e72155579167ba88
Masaya Nasu <tomato.wonder.life@gmail.com>
2016-04-08 02:03:21 +0900

    挨拶スクリプト追加

---
+++ scripts/hello.js
@@ -0,0 +1,32 @@
+// Description
+//  挨拶スクリプト
+//
+// Dependencies:
+//  なし
+//
+// Configuration:
+//  なし
+//
+// Commands:
+//  hibiki おはよう - 響がおはようを言ってくれます
+//  hibiki こんにちは - 響がこんにちはを言ってくれます
+//  hibiki こんばんは - 響がこんばんはを言ってくれます
+//
+// Notes:
+//  挨拶させたいWordを入れるべし
+//
+// Author:
+//  nasum
+export default function(robot){
+  robot.respond(/おはよう/, (msg) => {
+    msg.send("司令官、Доброе утро（ドーブラエ　ウートラ）. 眠いね。");
+  });
+
+  robot.respond(/こんにちは/, (msg) => {
+    msg.send("Добрый день（ドーブルイ　ディェン）. 司令官。お仕事お疲れ様。");
+  });
+
+  robot.respond(/こんばんは/, (msg) => {
+    msg.send("Добрый вечер（ドーブルイ　ヴィエーチル）. 司令官。おうちに帰りたいね。");
+  })
+}
