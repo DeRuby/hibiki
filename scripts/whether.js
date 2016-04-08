da2c802f2a84d24ac03e92cadcc9ce87b9b21143
Masaya Nasu <tomato.wonder.life@gmail.com>
2016-04-09 01:24:31 +0900

    天気予報スクリプトの追加

---
+++ scripts/whether.js
@@ -0,0 +1,30 @@
+// Description
+//  天気スクリプト
+//
+// Dependencies:
+//  なし
+//
+// Configuration:
+//  なし
+//
+// Commands:
+//  hibiki whether
+//
+// Notes:
+//  東京の天気を返す
+//
+// Author:
+//  nasum
+var fetch = require('node-fetch');
+export default function(robot){
+  robot.respond(/whether/, (msg) => {
+    fetch('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010')
+      .then(function(res){
+        return res.json();
+      }).then(function(json){
+        msg.send(json.title);
+        msg.send(json.description.text);
+        msg.send(json.link);
+      });
+  });
+}
